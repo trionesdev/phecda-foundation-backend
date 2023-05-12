3bd7bd5a7df4599327d2fce699f7942920bcda69
bane.shi <fengxiaotx@163.com>
2023-05-12 17:59:46 +0800

    feat: product thing model dev in device module

---
+++ core/src/main/java/ms/triones/backend/core/modules/device/dao/mapper/ProductThingModelDraftMapper.java
@@ -0,0 +1,7 @@
+package ms.triones.backend.core.modules.device.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import ms.triones.backend.core.modules.device.dao.entity.ProductThingModelDraft;
+
+public interface ProductThingModelDraftMapper extends BaseMapper<ProductThingModelDraft> {
+}
--- rest-backend/src/main/java/ms/triones/backend/rest/backend/modules/device/controller/impl/ProductController.java
+++ rest-backend/src/main/java/ms/triones/backend/rest/backend/modules/device/controller/impl/ProductController.java
@@ -3,6 +3,7 @@ package ms.triones.backend.rest.backend.modules.device.controller.impl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import ms.triones.backend.core.modules.device.dao.entity.ProductThingModelDraft;
 import ms.triones.backend.core.modules.device.service.bo.ThingModelUpsertBO;
 import ms.triones.backend.core.modules.device.service.impl.ProductService;
 import ms.triones.backend.rest.backend.modules.device.controller.ro.ProductThingModelUpsertRO;
@@ -10,6 +11,8 @@ import ms.triones.backend.rest.backend.modules.device.support.DeviceConstants;
 import ms.triones.backend.rest.backend.modules.device.support.DeviceRestConvertMapper;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Tag(name = "产品")
 @RequiredArgsConstructor
@@ -19,9 +22,17 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Operation(summary = "获取物模型草稿")
+    @GetMapping(value = "products/{productId}/thing-model")
+    public Optional<ProductThingModelDraft> findProductThingModelDraft(
+            @PathVariable(value = "productId") String productId
+    ) {
+        return productService.findProductThingModelDraft(productId);
+    }
+
     @Operation(summary = "新增物模型功能")
     @PutMapping(value = "products/{productId}/thing-model")
-    public void upsertThingModel(
+    public void upsertThingModelDraft(
             @PathVariable(value = "productId") String productId,
             @Validated @RequestBody ProductThingModelUpsertRO args
     ) {
