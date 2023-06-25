fb59ee88a8cb53e5b5934e9a39cce6f799428da2
bane.shi <fengxiaotx@163.com>
2023-06-25 18:08:29 +0800

    feat: product thing model dev

---
+++ core/src/main/java/ms/triones/backend/core/mqtt/MqttConfiguration.java
@@ -0,0 +1,18 @@
+package ms.triones.backend.core.mqtt;
+
+import lombok.RequiredArgsConstructor;
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class MqttConfiguration implements CommandLineRunner {
+    private final IMqttAsyncClient mqttAsyncClient;
+    private final MqttPhecdaMessageListener mqttPhecdaMessageListener;
+
+    @Override
+    public void run(String... args) throws Exception {
+        mqttAsyncClient.subscribe("/phecda/edge/message",1,mqttPhecdaMessageListener);
+    }
+}
