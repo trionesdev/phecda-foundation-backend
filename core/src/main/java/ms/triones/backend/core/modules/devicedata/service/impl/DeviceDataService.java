e55bb2ba2243547585a63cb62c66af864b8b8f20
bane.shi <fengxiaotx@163.com>
2023-06-28 18:01:50 +0800

    feat: iotdb sink

--- core/src/main/java/ms/triones/backend/core/modules/devicedata/service/impl/DeviceDataService.java
+++ core/src/main/java/ms/triones/backend/core/modules/devicedata/service/impl/DeviceDataService.java
@@ -1,20 +1,19 @@
 package ms.triones.backend.core.modules.devicedata.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import lombok.RequiredArgsConstructor;
+import ms.triones.backend.core.modules.devicedata.support.util.IotDbUtils;
 import org.apache.iotdb.rpc.IoTDBConnectionException;
 import org.apache.iotdb.rpc.StatementExecutionException;
-import org.apache.iotdb.session.SessionDataSet;
 import org.apache.iotdb.session.pool.SessionDataSetWrapper;
 import org.apache.iotdb.session.pool.SessionPool;
 import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
-import org.apache.iotdb.tsfile.read.common.Field;
-import org.apache.iotdb.tsfile.read.common.RowRecord;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,7 +23,7 @@ public class DeviceDataService {
 
     public void insertRecord(String nodeId, String deviceName, long time, List<String> measurements, List<TSDataType> types, List<Object> values) {
         try {
-            sessionPool.insertRecord("root.phecda.default.deveice1", time, measurements, types, values);
+            sessionPool.insertRecord(path(nodeId, deviceName), time, measurements, types, values);
         } catch (IoTDBConnectionException | StatementExecutionException e) {
             throw new RuntimeException(e);
         }
@@ -32,32 +31,42 @@ public class DeviceDataService {
 
     public void insertRecord(String nodeId, String deviceName, long time, List<String> measurements, List<String> values) {
         try {
-            sessionPool.insertRecord("root.phecda.default.deveice1", time, measurements, values);
+            sessionPool.insertRecord(path(nodeId, deviceName), time, measurements, values);
         } catch (IoTDBConnectionException | StatementExecutionException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public List<Map<String, String>> executeLastDataQuery(String nodeId, String deviceName) {
-        try (SessionDataSetWrapper sessionDataSet = sessionPool.executeQueryStatement("select last * from root.phecda.default.deveice1")) {
-            List<Map<String, String>> results = Lists.newArrayList();
-            List<String> columnNames = sessionDataSet.getColumnNames();
-            while (sessionDataSet.hasNext()) {
-                RowRecord rowRecord = sessionDataSet.next();
-                Map<String, String> row = Maps.newHashMap();
-                row.put(columnNames.get(0), String.valueOf(rowRecord.getTimestamp()));
-                if (!rowRecord.getFields().isEmpty()) {
-                    for (int i = 0; i < rowRecord.getFields().size(); i++) {
-                        row.put(columnNames.get(i + 1), rowRecord.getFields().get(i).getStringValue());
-                    }
-                }
-                results.add(row);
-            }
-            return results;
+    public List<Map<String, Object>> queryRawData(String nodeId, String deviceName,List<String> fields, long startTime, long endTime) {
+        String devicePath = path(nodeId, deviceName);
+        List<String> paths = fields.stream().map(field -> devicePath + "." + field).collect(Collectors.toList());
+        try (SessionDataSetWrapper sessionDataSet = sessionPool.executeRawDataQuery(paths, startTime, endTime, 6000)) {
+            return IotDbUtils.toList(sessionDataSet);
         } catch (IoTDBConnectionException | StatementExecutionException e) {
             throw new RuntimeException(e);
         }
-
     }
 
+    public List<Map<String, Object>> queryLastData(String nodeId, String deviceName, List<String> fields) {
+        String devicePath = path(nodeId, deviceName);
+        List<String> paths = fields.stream().map(field -> devicePath + "." + field).collect(Collectors.toList());
+        try (SessionDataSetWrapper sessionDataSet = sessionPool.executeLastDataQuery(paths)) {
+            return IotDbUtils.toList(sessionDataSet);
+        } catch (IoTDBConnectionException | StatementExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Map<String, Object>> queryLastData(String nodeId, String deviceName) {
+        try (SessionDataSetWrapper sessionDataSet = sessionPool.executeQueryStatement("select last * from " + path(nodeId, deviceName))) {
+            return IotDbUtils.toList(sessionDataSet);
+        } catch (IoTDBConnectionException | StatementExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private String path(String nodeId, String deviceName) {
+        return StrUtil.join(".", "root.phecda", nodeId, deviceName);
+    }
 }
