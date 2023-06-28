83030a94d0305835d72863e9eef423e165794d1a
bane.shi <fengxiaotx@163.com>
2023-06-28 18:01:50 +0800

    feat: iotdb sink

--- core/src/test/java/ms/triones/backend/core/modules/devicedata/service/impl/DeviceDataServiceTest.java
+++ core/src/test/java/ms/triones/backend/core/modules/devicedata/service/impl/DeviceDataServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
 
@@ -23,9 +24,24 @@ public class DeviceDataServiceTest extends BaseTest {
     }
 
     @Test
+    public void insert_test2() {
+        deviceDataService.insertRecord("default", "deveice1", Instant.now().toEpochMilli(),
+                Lists.newArrayList("status","maxTemp"), Lists.newArrayList("true","25"));
+        System.out.println("s");
+    }
+
+
+    @Test
+    public void queryRow_test(){
+        List<String> fields = Lists.newArrayList("maxTemp");
+        List<Map<String, Object>> res2 =  deviceDataService.queryRawData("default", "deveice1", fields,Instant.now().minus(2, ChronoUnit.DAYS).toEpochMilli(),Instant.now().plus(2, ChronoUnit.DAYS).toEpochMilli());
+        System.out.println(res2);
+    }
+
+    @Test
     public void queryLastRow_test() {
-        List<Map<String, String>> res = deviceDataService.executeLastDataQuery("default", "deveice1");
-        System.out.println(res);
+        List<Map<String, Object>> res2 = deviceDataService.queryLastData("default", "deveice1",Lists.newArrayList("maxTemp"));
+        System.out.println(res2);
     }
 
 }
