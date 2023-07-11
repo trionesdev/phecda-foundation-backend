fe9d93d433b53bc7bd84dcdc055ab912a310c1e8
王远 <wangyuan2185@163.com>
2023-07-11 13:58:21 +0800

    Accept Merge Request #39: (wy-linkage-dev-20230711 -> develop) Merge Request: 增加场景分页接口
    Created By: @王远
    Accepted By: @王远
    URL: https://jscoe.coding.net/p/belt/d/belt-backend/git/merge/39?initial=true

---
+++ rest-backend/src/main/java/ms/triones/backend/rest/backend/modules/linkage/controller/query/LinkageSceneQuery.java
@@ -0,0 +1,13 @@
+package ms.triones.backend.rest.backend.modules.linkage.controller.query;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class LinkageSceneQuery {
+    private String name;
+
+    private Integer pageNum;
+    private Integer pageSize;
+}
