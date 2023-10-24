56bb5c9998cf5bb196ff3b53fccdfc0e4a6e1b8f
wang yuan <wangyuan2185@163.com>
2023-10-24 08:15:56 +0000

    增加rest sdk

---
+++ rest-ssp-sdk/src/main/java/ms/phecda/rest/ssp/sdk/device/thing/model/ThingModelProperty.java
@@ -0,0 +1,31 @@
+package ms.phecda.rest.ssp.sdk.device.thing.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import ms.phecda.rest.ssp.sdk.device.valuetype.ValueType;
+import ms.phecda.rest.ssp.sdk.device.valuetype.ValueTypeEnum;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ThingModelProperty extends ThingModelAbility {
+    private ValueTypeEnum valueType;
+    private ValueType valueSpec;
+    private List<ValueType> valueSpecs;
+    private Rw rw;
+    private Boolean required;
+    private Boolean custom;
+
+    public enum Rw {
+        R,
+        W,
+        RW
+    }
+}
