99d6873f9c70ba479e9582cd7315c8d2310e1dbb
Bane.Shi <fengxiaotx@163.com>
2024-04-18 15:19:49 +0800

    feat: action trigger

--- domain/domain-core/src/main/java/ms/phecda/backend/core/domains/device/dao/impl/DeviceStatisticsMessageDailyDAO.java
+++ domain/domain-core/src/main/java/ms/phecda/backend/core/domains/device/dao/impl/DeviceStatisticsMessageDailyDAO.java
@@ -8,6 +8,7 @@ import ms.phecda.backend.core.domains.device.dao.entity.DeviceStatisticsMessageD
 import ms.phecda.backend.core.domains.device.dao.mapper.DeviceStatisticsMessageDailyMapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
@@ -19,7 +20,10 @@ public class DeviceStatisticsMessageDailyDAO extends ServiceImpl<DeviceStatistic
         LambdaQueryWrapper<DeviceStatisticsMessageDaily> queryWrapper = new LambdaQueryWrapper<>();
         if (Objects.nonNull(criteria)) {
             queryWrapper.ge(Objects.nonNull(criteria.getStartTime()), DeviceStatisticsMessageDaily::getDate, criteria.getStartTime())
-                    .le(Objects.nonNull(criteria.getEndTime()), DeviceStatisticsMessageDaily::getDate, criteria.getEndTime());
+                    .le(Objects.nonNull(criteria.getEndTime()), DeviceStatisticsMessageDaily::getDate, criteria.getEndTime())
+                    .eq(Objects.nonNull(criteria.getType()), DeviceStatisticsMessageDaily::getType, criteria.getType())
+                    .eq(Objects.nonNull(criteria.getDate()), DeviceStatisticsMessageDaily::getDate, criteria.getDate())
+            ;
         }
         return queryWrapper;
     }
@@ -36,6 +40,10 @@ public class DeviceStatisticsMessageDailyDAO extends ServiceImpl<DeviceStatistic
     public Long selectQuantitySum(DeviceStatisticsMessageDailyCriteria criteria) {
         Map<String, Object> map = this.getMap(buildQueryWrapper(criteria).select(" sum(quantity) as totalQuantity "));
         return Optional.ofNullable(map.get("totalQuantity")).map(t -> Long.parseLong(t.toString())).orElse(0L);
+    }
+
+    public List<DeviceStatisticsMessageDaily> selectList(DeviceStatisticsMessageDailyCriteria criteria) {
+        return this.baseMapper.selectList(buildLambdaQueryWrapper(criteria));
     }
 
 }
--- serve/src/main/java/ms/phecda/ServeApplication.java
+++ serve/src/main/java/ms/phecda/ServeApplication.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @SpringBootApplication
+@EnableScheduling
 @MapperScan(value = {"ms.phecda.backend.core.domains.*.dao.mapper"})
 @EnableMinioClients
 public class ServeApplication {
