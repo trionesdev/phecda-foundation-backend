477c28bab42eef2dd9ef14d4c46066b582125ab4
落雁沙 <fengxiaotx@163.com>
2025-01-05 20:19:51 +0800

    feat: docker build

--- docker.sh
+++ docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# 本地构建docker镜像并推送到仓库
 IMAGE_NAME=registry.cn-shanghai.aliyuncs.com/epiboly/demo
 TAG=$(git rev-parse HEAD)
 
 mvn clean package -U -Dmaven.test.skip=true
 
 docker build -f Dockerfile --force-rm=true --rm -t ${IMAGE_NAME}:"${TAG}" .
+
+docker push ${IMAGE_NAME}:"${TAG}"
