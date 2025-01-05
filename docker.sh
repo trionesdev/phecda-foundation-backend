430cb771dddc443fdec8cdea4eaf6cdce38d82c0
落雁沙 <fengxiaotx@163.com>
2025-01-05 20:21:35 +0800

    feat: docker build

--- docker.sh
+++ docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # 本地构建docker镜像并推送到仓库
+APP_NAME=phecda-foundation-backend
 IMAGE_NAME=registry.cn-shanghai.aliyuncs.com/epiboly/demo
-TAG=$(git rev-parse HEAD)
+TAG=${APP_NAME}-$(git rev-parse HEAD)
 
 mvn clean package -U -Dmaven.test.skip=true
 
