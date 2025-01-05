5faa7727fcec5f9e5a3aee25e54f4211b3f6ed2b
落雁沙 <fengxiaotx@163.com>
2025-01-05 10:19:35 +0800

    feat:

--- interfaces/rest-tenant/src/main/java/com/trionesdev/phecda/foundation/rest/tenant/internal/RestConstants.java
+++ interfaces/rest-tenant/src/main/java/com/trionesdev/phecda/foundation/rest/tenant/internal/RestConstants.java
@@ -1,5 +1,5 @@
 package com.trionesdev.phecda.foundation.rest.tenant.internal;
 
 public class RestConstants {
-    public final static String CONTEXT_PATH = "tenant-api/be/";
+    public final static String CONTEXT_PATH = "tenant-api/";
 }
